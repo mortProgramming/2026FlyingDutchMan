d786eeedf42f1e3ac5009969a64f70110cde08d9
Your Name <your_email@example.com>
2025-12-15 20:15:11 -0500

    Sammy and Anthony worked on autobuilder

--- src/main/java/frc/robot/configs/constants/PIDconstants.java
+++ src/main/java/frc/robot/configs/constants/PIDconstants.java
@@ -3,6 +3,34 @@ package frc.robot.configs.constants;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 
 public class PIDconstants {
+	public final class Drivetrain{
+		public static final double AUTON_POS_KP = 1;
+		public static final double AUTON_POS_KI = 0;
+		// public static final double AUTON_POS_KD = 0.001x;
+		public static final double AUTON_POS_KD = 0.16;
+	
+		public static final double AUTON_ROTATION_KP = 2;
+		// public static final double AUTON_ROTATION_KP = 100;
+		public static final double AUTON_ROTATION_KI = 0.;
+		public static final double AUTON_ROTATION_KD = 0;
+
+		public final static double POS_KP = 3;
+		public final static double POS_KI = 0;
+		public final static double POS_KD = 0;
+		public static final Constraints POS_CONSTRAINTS = new Constraints(1, 3);
+		public final static double POS_POS_TOLERANCE = 0.03;
+
+		public final static double ANGLE_KP = 0.07;
+		public final static double ANGLE_KI = 0;
+		public final static double ANGLE_KD = 0;
+		public static final Constraints ANGLE_CONSTRAINTS = new Constraints(100, 180);
+		// public static final Constraints ANGLE_CONSTRAINTS = new Constraints(50, 1000);
+		public final static double ANGLE_POS_TOLERANCE = 5;
+		public final static double ANGLE_VEL_TOLERANCE = 20;
+
+		public final static double TRANSLATIONAL_SLEW_LIMIT = 5;
+		public final static double ROTATIONAL_SLEW_LIMIT = 10;
+	}
     public final class Elevator {
 		public static final double POS_KP = 0.08;
 		public static final double POS_KI = 0;
@@ -19,5 +47,6 @@ public class PIDconstants {
 		public static final double POS_KG = -0.05;
 		public static final double POS_KV = 0;
 		public static final double POS_KA = 0;
+	
   }
 }
