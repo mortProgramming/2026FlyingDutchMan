2a98ae8e48f9a857a2842e83e2c268d33533a0b6
Your Name <your_email@example.com>
2026-01-21 19:27:34 -0500

    Continued align to limelight sammy r u proud of me

--- src/main/java/frc/robot/commands/autons/BetterAlignToTag.java
+++ src/main/java/frc/robot/commands/autons/BetterAlignToTag.java
@@ -1,17 +1,34 @@
 package frc.robot.commands.autons;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Vision;
 
 public class BetterAlignToTag extends Command{
+    
     private Vision vision;
     private CommandSwerveDrivetrain drivetrain;
     private int tagID;
 
+    private boolean tagSeen;
     
     public BetterAlignToTag(){
+        vision = vision.getInstance();
 
+        drivetrain = RobotContainer.getSwerveDrivetrain();
+
+        tagID = vision.getTagId();
+
+        tagSeen = false; //when  the camera sees the tag, this will be changed to true
+        addRequirements(vision, drivetrain);
+    }
+
+    //local method 
+    public void tagSeen(){
+        if(vision.hasTag()){
+            tagSeen = true;
+        }
     }
     
 }
