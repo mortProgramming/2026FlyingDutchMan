8abbee52527720a0ed51c23cd93bee9b3102e209
Your Name <your_email@example.com>
2026-01-28 19:28:58 -0500

    Random stuff

--- src/main/java/frc/robot/commands/autons/PathFind.java
+++ src/main/java/frc/robot/commands/autons/PathFind.java
@@ -2,7 +2,6 @@ package frc.robot.commands.autons;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathConstraints;
-import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -42,15 +41,36 @@ public class PathFind extends Command {
         this.constraints = constraints;
         addRequirements(drivetrain, vision);
     }
+    
+    // public PathFind(CommandSwerveDrivetrain drivetrain, Pose2d startPose, Pose2d endPose, PathConstraints constraints,
+    //         Vision vision) {
+    //     this.drivetrain = drivetrain;
+    //     this.startPose = drivetrain.getPose();
+    //     this.endPose = endPose;
+    //     this.constraints = constraints;
+    //     addRequirements(drivetrain, vision);
+    // }  This constructor will use linear vel;ocity will prob get rid of other one 
+    
+    //PSUEDOCODE:
+    /* when this command runs, get the initial pose of the robot and then run the path generation based on given values, 
+    for the target pose.  Then get the robots ACTUAL end pose and correct it in the GUI by running some random update command.
+    (IDK if im going to literally correct the endPOse to TargetPose OR just update the true pose and NOT the targetPose but the robots 
+    Actual POSE) */
 
+    public Command pathFindMethod(){
+        Command pathfindingCommand = AutoBuilder.pathfindToPose(this.targetPose,this.constraints,0.0); //for now it is zero
+        return pathfindingCommand;
+    }
     @Override
-    public void initialize() {
+    public void initialize(){
+        
     }
 
     @Override
-    public void execute() {
-    }
+    public void execute(){
 
+    }
+    
     @Override
     public void end(boolean interrupted) {
         // when the path that was generated on the fly ends, update the robot pose with
